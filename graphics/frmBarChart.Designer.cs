8656d4665279d70df094f78d7e3307ab464a9e8d
Alexander D. Martinez <alekisan@gmail.com>
2013-08-21 08:30:35 -0400

    Initial upload

---
+++ graphics/frmBarChart.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace graphics
+{
+    partial class frmBarChart
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpBoxScores = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.grpBoxActions = new System.Windows.Forms.GroupBox();
+            this.btnClearAll = new System.Windows.Forms.Button();
+            this.btnDrawChart = new System.Windows.Forms.Button();
+            this.grpBoxColors = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnBarColor4 = new System.Windows.Forms.Button();
+            this.btnBarColor3 = new System.Windows.Forms.Button();
+            this.btnBarColor2 = new System.Windows.Forms.Button();
+            this.btnBarColor1 = new System.Windows.Forms.Button();
+            this.pnlDrawArea = new System.Windows.Forms.Panel();
+            this.myColorPicker = new System.Windows.Forms.ColorDialog();
+            this.grpBoxScores.SuspendLayout();
+            this.grpBoxActions.SuspendLayout();
+            this.grpBoxColors.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpBoxScores
+            // 
+            this.grpBoxScores.Controls.Add(this.label4);
+            this.grpBoxScores.Controls.Add(this.label3);
+            this.grpBoxScores.Controls.Add(this.label2);
+            this.grpBoxScores.Controls.Add(this.label1);
+            this.grpBoxScores.Controls.Add(this.textBox4);
+            this.grpBoxScores.Controls.Add(this.textBox3);
+            this.grpBoxScores.Controls.Add(this.textBox2);
+            this.grpBoxScores.Controls.Add(this.textBox1);
+            this.grpBoxScores.Location = new System.Drawing.Point(418, 12);
+            this.grpBoxScores.Name = "grpBoxScores";
+            this.grpBoxScores.Size = new System.Drawing.Size(200, 284);
+            this.grpBoxScores.TabIndex = 0;
+            this.grpBoxScores.TabStop = false;
+            this.grpBoxScores.Text = "Scores";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(37, 212);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Player 4";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 157);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Player 3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Player 2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Player 1";
+            // 
+            // textBox4
+            // 
+            this.textBox4.ForeColor = System.Drawing.Color.DarkGoldenrod;
+            this.textBox4.Location = new System.Drawing.Point(37, 228);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.ForeColor = System.Drawing.Color.Green;
+            this.textBox3.Location = new System.Drawing.Point(37, 173);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.ForeColor = System.Drawing.Color.Blue;
+            this.textBox2.Location = new System.Drawing.Point(37, 111);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.ForeColor = System.Drawing.Color.Red;
+            this.textBox1.Location = new System.Drawing.Point(37, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // grpBoxActions
+            // 
+            this.grpBoxActions.Controls.Add(this.btnClearAll);
+            this.grpBoxActions.Controls.Add(this.btnDrawChart);
+            this.grpBoxActions.Location = new System.Drawing.Point(418, 302);
+            this.grpBoxActions.Name = "grpBoxActions";
+            this.grpBoxActions.Size = new System.Drawing.Size(200, 100);
+            this.grpBoxActions.TabIndex = 1;
+            this.grpBoxActions.TabStop = false;
+            this.grpBoxActions.Text = "Actions";
+            // 
+            // btnClearAll
+            // 
+            this.btnClearAll.Location = new System.Drawing.Point(63, 60);
+            this.btnClearAll.Name = "btnClearAll";
+            this.btnClearAll.Size = new System.Drawing.Size(75, 23);
+            this.btnClearAll.TabIndex = 1;
+            this.btnClearAll.Text = "Clear";
+            this.btnClearAll.UseVisualStyleBackColor = true;
+            this.btnClearAll.Click += new System.EventHandler(this.btnClearAll_Click);
+            // 
+            // btnDrawChart
+            // 
+            this.btnDrawChart.AutoSize = true;
+            this.btnDrawChart.Location = new System.Drawing.Point(56, 19);
+            this.btnDrawChart.Name = "btnDrawChart";
+            this.btnDrawChart.Size = new System.Drawing.Size(89, 23);
+            this.btnDrawChart.TabIndex = 0;
+            this.btnDrawChart.Text = "Draw Bar Chart";
+            this.btnDrawChart.UseVisualStyleBackColor = true;
+            this.btnDrawChart.Click += new System.EventHandler(this.btnDrawChart_Click);
+            // 
+            // grpBoxColors
+            // 
+            this.grpBoxColors.Controls.Add(this.label5);
+            this.grpBoxColors.Controls.Add(this.btnBarColor4);
+            this.grpBoxColors.Controls.Add(this.btnBarColor3);
+            this.grpBoxColors.Controls.Add(this.btnBarColor2);
+            this.grpBoxColors.Controls.Add(this.btnBarColor1);
+            this.grpBoxColors.Location = new System.Drawing.Point(418, 408);
+            this.grpBoxColors.Name = "grpBoxColors";
+            this.grpBoxColors.Size = new System.Drawing.Size(200, 100);
+            this.grpBoxColors.TabIndex = 1;
+            this.grpBoxColors.TabStop = false;
+            this.grpBoxColors.Text = "Bar Colors";
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(20, 55);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(161, 29);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Click these buttons to change the colors of the bars.";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // btnBarColor4
+            // 
+            this.btnBarColor4.Location = new System.Drawing.Point(151, 19);
+            this.btnBarColor4.Name = "btnBarColor4";
+            this.btnBarColor4.Size = new System.Drawing.Size(29, 23);
+            this.btnBarColor4.TabIndex = 4;
+            this.btnBarColor4.Text = "4";
+            this.btnBarColor4.UseVisualStyleBackColor = true;
+            this.btnBarColor4.Click += new System.EventHandler(this.btnBarColor4_Click);
+            // 
+            // btnBarColor3
+            // 
+            this.btnBarColor3.Location = new System.Drawing.Point(107, 19);
+            this.btnBarColor3.Name = "btnBarColor3";
+            this.btnBarColor3.Size = new System.Drawing.Size(29, 23);
+            this.btnBarColor3.TabIndex = 3;
+            this.btnBarColor3.Text = "3";
+            this.btnBarColor3.UseVisualStyleBackColor = true;
+            this.btnBarColor3.Click += new System.EventHandler(this.btnBarColor3_Click);
+            // 
+            // btnBarColor2
+            // 
+            this.btnBarColor2.Location = new System.Drawing.Point(63, 19);
+            this.btnBarColor2.Name = "btnBarColor2";
+            this.btnBarColor2.Size = new System.Drawing.Size(29, 23);
+            this.btnBarColor2.TabIndex = 2;
+            this.btnBarColor2.Text = "2";
+            this.btnBarColor2.UseVisualStyleBackColor = true;
+            this.btnBarColor2.Click += new System.EventHandler(this.btnBarColor2_Click);
+            // 
+            // btnBarColor1
+            // 
+            this.btnBarColor1.Location = new System.Drawing.Point(19, 19);
+            this.btnBarColor1.Name = "btnBarColor1";
+            this.btnBarColor1.Size = new System.Drawing.Size(29, 23);
+            this.btnBarColor1.TabIndex = 1;
+            this.btnBarColor1.Text = "1";
+            this.btnBarColor1.UseVisualStyleBackColor = true;
+            this.btnBarColor1.Click += new System.EventHandler(this.btnBarColor1_Click);
+            // 
+            // pnlDrawArea
+            // 
+            this.pnlDrawArea.Location = new System.Drawing.Point(13, 12);
+            this.pnlDrawArea.Name = "pnlDrawArea";
+            this.pnlDrawArea.Size = new System.Drawing.Size(399, 500);
+            this.pnlDrawArea.TabIndex = 2;
+            // 
+            // myColorPicker
+            // 
+            this.myColorPicker.AllowFullOpen = false;
+            // 
+            // frmBarChart
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(630, 520);
+            this.Controls.Add(this.pnlDrawArea);
+            this.Controls.Add(this.grpBoxColors);
+            this.Controls.Add(this.grpBoxActions);
+            this.Controls.Add(this.grpBoxScores);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "frmBarChart";
+            this.Text = "500 Scores";
+            this.grpBoxScores.ResumeLayout(false);
+            this.grpBoxScores.PerformLayout();
+            this.grpBoxActions.ResumeLayout(false);
+            this.grpBoxActions.PerformLayout();
+            this.grpBoxColors.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpBoxScores;
+        private System.Windows.Forms.GroupBox grpBoxActions;
+        private System.Windows.Forms.GroupBox grpBoxColors;
+        private System.Windows.Forms.Panel pnlDrawArea;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnClearAll;
+        private System.Windows.Forms.Button btnDrawChart;
+        private System.Windows.Forms.Button btnBarColor4;
+        private System.Windows.Forms.Button btnBarColor3;
+        private System.Windows.Forms.Button btnBarColor2;
+        private System.Windows.Forms.Button btnBarColor1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ColorDialog myColorPicker;
+    }
+}
+
---
+++ graphics/frmBarChart.cs
@@ -0,0 +1,298 @@
+﻿/* 
+ * Name: Alexander D. Martinez
+ * Date: 11/24/09
+ * Purpose: Learn to use form graphics capabilities. 
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Text;
+using System.Windows.Forms;
+
+namespace graphics
+{
+    public partial class frmBarChart : Form
+    {
+        //
+        // init area
+        //
+
+        int x1 = 0;
+        int y1 = 0;
+        int w1 = 0;
+        int h1 = 0;
+
+        int x2 = 0;
+        int y2 = 0;
+        int w2 = 0;
+        int h2 = 0;
+
+        int x3 = 0;
+        int y3 = 0;
+        int w3 = 0;
+        int h3 = 0;
+
+        int x4 = 0;
+        int y4 = 0;
+        int w4 = 0;
+        int h4 = 0;
+
+        private Color p1Color = Color.Red;
+        private Color p2Color = Color.Blue;
+        private Color p3Color = Color.Green;
+        private Color p4Color = Color.Goldenrod;
+
+        public frmBarChart()
+        {
+            InitializeComponent();
+        }
+
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics grObj = pnlDrawArea.CreateGraphics();
+
+            Pen linePen = new Pen(Color.Black, 3);
+            SolidBrush numBrush = new SolidBrush(Color.Black);
+            SolidBrush p1Brush = new SolidBrush(p1Color);
+            SolidBrush p2Brush = new SolidBrush(p2Color);
+            SolidBrush p3Brush = new SolidBrush(p3Color);
+            SolidBrush p4Brush = new SolidBrush(p4Color);
+            
+            
+            FontStyle fStyle = FontStyle.Regular;
+            Font sansSerif = new Font("Microsoft Sans Serif", 8, fStyle);
+
+            grObj.Clear(frmBarChart.DefaultBackColor);
+
+            //draw bar chart
+            grObj.FillRectangle(p1Brush, x1, y1, w1, h1);
+            grObj.FillRectangle(p2Brush, x2, y2, w2, h2);
+            grObj.FillRectangle(p3Brush, x3, y3, w3, h3);
+            grObj.FillRectangle(p4Brush, x4, y4, w4, h4);
+
+            //draw grid
+            grObj.DrawLine(linePen, 0, 0, 0, 500);
+            grObj.DrawLine(linePen, 0, 0, 10, 0);
+            grObj.DrawLine(linePen, 0, 100, 10, 100);
+            grObj.DrawLine(linePen, 0, 200, 10, 200);
+            grObj.DrawLine(linePen, 0, 300, 10, 300);
+            grObj.DrawLine(linePen, 0, 400, 10, 400);
+            grObj.DrawString("500", sansSerif, numBrush, 11, 0);
+            grObj.DrawString("400", sansSerif, numBrush, 11, 100);
+            grObj.DrawString("300", sansSerif, numBrush, 11, 200);
+            grObj.DrawString("200", sansSerif, numBrush, 11, 300);
+            grObj.DrawString("100", sansSerif, numBrush, 11, 400);
+            
+        }
+        //
+        // init area
+        //
+        //-----------------------------------------------------------------------
+        //
+        // Draw button execute
+        //
+        private void btnDrawChart_Click(object sender, EventArgs e)
+        {
+            int score1;
+            int score2;
+            int score3;
+            int score4;
+
+            if (checkNumber(textBox1.Text))
+            {
+                score1 = Convert.ToInt32(textBox1.Text);
+                makeBarChart(1, score1);
+            }
+            else
+            {
+                MessageBox.Show("Score must be >= 0 and <= 500", "Player 1 out of range", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+            if (checkNumber(textBox2.Text))
+            {
+                score2 = Convert.ToInt32(textBox2.Text);
+                makeBarChart(2, score2); 
+            }
+            else
+            {
+                MessageBox.Show("Score must be >= 0 and <= 500", "Player 2 out of range", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+            if (checkNumber(textBox3.Text))
+            {
+                score3 = Convert.ToInt32(textBox3.Text);
+                makeBarChart(3, score3); 
+            }
+            else
+            {
+                MessageBox.Show("Score must be >= 0 and <= 500", "Player 3 out of range", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+            if (checkNumber(textBox4.Text))
+            {
+                score4 = Convert.ToInt32(textBox4.Text);
+                makeBarChart(4, score4); 
+            }
+            else
+            {
+                MessageBox.Show("Score must be >= 0 and <= 500", "Player 4 out of range", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            Invalidate();
+        }
+        //
+        // Draw button execute
+        //
+        //----------------------------------------------------------------------
+        //
+        // clear display
+        //        
+        private void btnClearAll_Click(object sender, EventArgs e)
+        {
+            x1 = 0;
+            y1 = 0;
+            w1 = 0;
+            h1 = 0;
+
+            x2 = 0;
+            y2 = 0;
+            w2 = 0;
+            h2 = 0;
+
+            x3 = 0;
+            y3 = 0;
+            w3 = 0;
+            h3 = 0;
+
+            x4 = 0;
+            y4 = 0;
+            w4 = 0;
+            h4 = 0;
+
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+
+            Invalidate();
+        }
+        //
+        // clear display
+        //
+        //----------------------------------------------------------------------
+        //
+        // change colors
+        //
+        private void btnBarColor1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = myColorPicker.ShowDialog();
+
+            if (result == DialogResult.Cancel)
+                return;
+
+            textBox1.ForeColor = myColorPicker.Color;
+            p1Color = myColorPicker.Color;
+            Invalidate();
+        }
+
+        private void btnBarColor2_Click(object sender, EventArgs e)
+        {
+            DialogResult result = myColorPicker.ShowDialog();
+
+            if (result == DialogResult.Cancel)
+                return;
+
+            textBox2.ForeColor = myColorPicker.Color;
+            p2Color = myColorPicker.Color;
+            Invalidate();
+        }
+
+        private void btnBarColor3_Click(object sender, EventArgs e)
+        {
+            DialogResult result = myColorPicker.ShowDialog();
+
+            if (result == DialogResult.Cancel)
+                return;
+
+            textBox3.ForeColor = myColorPicker.Color;
+            p3Color = myColorPicker.Color;
+            Invalidate();
+        }
+
+        private void btnBarColor4_Click(object sender, EventArgs e)
+        {
+            DialogResult result = myColorPicker.ShowDialog();
+
+            if (result == DialogResult.Cancel)
+                return;
+
+            textBox4.ForeColor = myColorPicker.Color;
+            p4Color = myColorPicker.Color;
+            Invalidate();
+        }
+        //
+        // change colors
+        //
+        //----------------------------------------------------------------------------
+        //
+        // Methods
+        //
+        // Creates the chart
+        private void makeBarChart(int player, int height)
+        {
+            if (player == 1)
+            {
+                x1 = 50;
+                y1 = 500 - height;
+                w1 = 70;
+                h1 = height; 
+            }
+
+            if (player == 2)
+            {
+                x2 = 140;
+                y2 = 500 - height;
+                w2 = 70;
+                h2 = height;
+            }
+
+            if (player == 3)
+            {
+                x3 = 230;
+                y3 = 500 - height;
+                w3 = 70;
+                h3 = height;
+            }
+
+            if (player == 4)
+            {
+                x4 = 320;
+                y4 = 500 - height;
+                w4 = 70;
+                h4 = height;
+            }
+        }
+
+        // verifies input
+        private bool checkNumber(string input)
+        {
+            int number = Convert.ToInt32(input);
+            bool result = false;
+
+            if (number >= 0 && number <= 500)
+            {
+                result = true;
+            }
+
+            return result;
+        }
+
+    }
+}
